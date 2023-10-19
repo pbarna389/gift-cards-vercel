dfde37edd5c5b6a2117c13e91e636dfca996932d
pbarna389 <64441402+pbarna389@users.noreply.github.com>
2023-10-19 19:55:40 +0200

    rollup removed

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,10 @@
 // https://vitejs.dev/config/
 import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
-import replace from '@rollup/plugin-replace'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
-	plugins: [
-		react(),
-		tsconfigPaths(),
-		{
-			...replace({
-				'process.env.REACT_APP_API_KEY': JSON.stringify(
-					process.env.REACT_APP_API_KEY
-				),
-			}),
-			enforce: 'pre',
-		},
-	],
+	plugins: [react(), tsconfigPaths()],
 	optimizeDeps: { exclude: ['swiper/react', 'swiper/types'] },
 	test: {
 		globals: true,
